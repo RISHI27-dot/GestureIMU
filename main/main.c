af8bab1bb5fefdd93af306b22e9bd76fc2b2c403
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2024-03-15 21:05:26 +0530

    Add BLE setup as component

--- main/main.c
+++ main/main.c
@@ -5,11 +5,14 @@
 
 #include "mpu.h"
 #include "sdkconfig.h"
+#include "esp_hid_gap.h"
+#include "esp_hid_device.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 static const char *TAG = "mpu_example";
+struct acc_values *data;
 
 void app_main()
 {
@@ -20,11 +23,18 @@ void app_main()
       {
           // Setting up the result arrays, time difference and initial conditions
           int16_t acce_raw_value[BUFF_SIZE / 2], gyro_raw_value[BUFF_SIZE / 2];
+          setup(); // Setup BLE
           while (read_mpu_raw(acce_raw_value, gyro_raw_value) == ESP_OK)
           {
             ESP_LOGI(TAG, "acce_raw_value = %d %d %d",acce_raw_value[0],acce_raw_value[1],acce_raw_value[2]);
+            data = malloc(sizeof(struct acc_values));
+            data->ax = acce_raw_value[0];
+            data->ay = acce_raw_value[1];
+            data->az = acce_raw_value[2];
+            ble_hid_task(data);
+            free(data);
           }
-        }
+      }
     }
 
     ESP_LOGE(TAG, "%s", "Error reading values!");
